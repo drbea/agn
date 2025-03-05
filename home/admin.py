368be9aa946f08fdd408f2c7e35df5f6b375c227
drbea <beavoguimaximeakoi@gmail.com>
2025-03-05 23:36:57 +0000

    ajoutt de model de temoignage

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Testimonial
 
 # Register your models here.
+
+
+
+@admin.register(Testimonial)
+class TestimonialAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at', 'approved')  # Colonnes à afficher dans la liste
+    list_filter = ('approved',)  # Permet de filtrer par témoignages approuvés
+    search_fields = ('name', 'message')  # Recherche par nom ou message
+    actions = ['approve_testimonials']  # Ajouter une action personnalisée
+
+    # Action pour approuver les témoignages
+    def approve_testimonials(self, request, queryset):
+        queryset.update(approved=True)
+    approve_testimonials.short_description = 'Approuver les témoignages sélectionnés'
