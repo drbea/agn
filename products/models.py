88902e00a2668e45ab415e769676eda521e60e1c
drbea <beavoguimaximeakoi@gmail.com>
2025-03-05 23:26:49 +0000

    ajoutt de la section detail dun produit, mse a jour de la barre de recherche, de la filtration par categorie

--- products/models.py
+++ products/models.py
@@ -11,7 +11,7 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
-    
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
@@ -25,10 +25,6 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
-
-    # def count_reactions(self):
-    #     """Retourne un dictionnaire avec le nombre total de chaque type de réaction."""
-    #     return self.reactions.values('type_reaction').annotate(count=models.Count('type_reaction'))
 
 
 # class Commentaire(models.Model):
@@ -45,23 +41,16 @@ class Product(models.Model):
 #         return self.contenu[:50]
 
 
-# class Reaction(models.Model):
-#     autheur = models.ForeignKey(User, on_delete=models.CASCADE)
-#     produit = models.ForeignKey(Publication, on_delete=models.CASCADE, related_name='reactions')
-#     type_reaction = models.CharField(
-#         max_length=50,
-#         choices=[
-#             ("like", "like"),
-#             ("dislike", "dislike"),
-#         ]
-#     )
-#     date_reaction = models.DateTimeField(auto_now=True)
-
-#     class Meta:
-#         unique_together = ('autheur', 'produit', 'type_reaction')  # Empêche plusieurs réactions du même type par utilisateur/publication
-
-#     def __str__(self):
-#         return f"{self.autheur} a réagi sur {self.produit} avec {self.type_reaction}"
 
 
-
+### AJOUTER PLUSIEUR images
+# class Product(models.Model):
+#     name = models.CharField(max_length=255)
+#     description = models.TextField()
+#     price = models.DecimalField(max_digits=10, decimal_places=2)
+#     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+#     images = models.ManyToManyField(Image)  # Lien avec la table Image
+#
+# class Image(models.Model):
+#     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+#     image = models.ImageField(upload_to='product_images/')
--- products/urls.py
+++ products/urls.py
@@ -6,5 +6,9 @@ app_name = "products"
 urlpatterns = [
   path("", views.product_list, name = "product_list"),
   path("all/", views.product_list, name = "product_list"),
+  path("add/", views.add_product, name = "add_product"),
+
+  path("detail/<int:product_id>/", views.product_detail, name = "product_detail"),
+  path("filter/<int:category_id>/", views.filter_by_category, name = "filter_by_category"),
 
 ]
